300cbb196fef0db8f367d355562d10c81d89f963
Marcel Otavio Cerri <32442095+marcelcerri@users.noreply.github.com>
2020-04-15 20:23:46 -0300

    Inserção de novos dados e integração
    Novos dados de k e temperatura, integração numérica da reação

--- Arrhenius.py
+++ Arrhenius.py
@@ -1,4 +1,3 @@
-
 # Ajustes linear da dados experimentais de Arrhenius
 
 # importando os pacotes do python
@@ -8,7 +7,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Lendo o arquivo csv na pasta
-dados = pd.read_csv('Arrhenius.csv')
+dados = pd.read_csv('dados_livro_arrhenius.csv')
 
 #transformando data frame em vetor
 dados_np = dados.values
@@ -48,19 +47,12 @@ dados_df = pd.DataFrame({"Temperatura":dados_np[:,0], "velocidade_mod":dados_v, 
 print(dados_df)
 
 print(f'A energia de ativação é: {Ea:9.7} Joules/mol')
-print(f'O fator de frequencia é: {A:9.5} 1/s')
+print(f'O fator de frequencia é: {A:9.5} dm^3/mol.s')
 print(f'O Coeficiente de ajuste é: {r2:5.5}')
 print(50*"_")
 
 
 # Ajustes não linear da dados experimentais de Arrhenius
-
-# importando os pacotes do python
-# Lendo o arquivo csv na pasta
-dados = pd.read_csv('Arrhenius.csv')
-
-#transformando data frame em vetor
-dados_np = dados.values
 
 #vetor do eixo x
 x = dados_np[:, 0]
@@ -97,11 +89,44 @@ dados_df = pd.DataFrame(
 print(dados_df)
 
 print(f'A energia de ativação é: {p2:9.7} Joules/mol')
-print(f'O fator de frequencia é: {p1:9.5} 1/s')
+print(f'O fator de frequencia é: {p1:9.5} dm^3/mol.s')
 print(f'O Coeficiente de ajuste é: {r2:5.4}')
 print(50*"_")
 
 
+# Integrando reação A + B - C + D
+from scipy.integrate import odeint
 
+# reação química de segunda ordem, reagentes A e B, produtos C e D
+def reacao (C,t):
+    Ca , Cb, Cc, Cd = C
+    k = 0.001078
+    dCadt = -k*Ca*Cb
+    dCbdt = -k*Ca*Cb
+    dCcdt = k*Ca*Cb
+    dCddt = k*Ca*Cb
+    return [dCadt,dCbdt, dCcdt, dCddt]
 
+# tempo e concentração inicial
+t = np.arange (0,1000,10)
+C0 = [7,6,1,0]
 
+#integração numérica
+C = odeint(reacao, C0, t)
+
+#Construindo o gráficos
+plt.plot(t, C[:,0], 'r-',linewidth=2, label="Ca")
+plt.plot(t, C[:,1],'b-',linewidth=2, label="Cb")
+plt.plot(t, C[:,2], 'g--',linewidth=2, label="Cc")
+plt.plot(t, C[:,3], 'k--',linewidth=2, label="Cd")
+plt.xlabel("tempo (s)")
+plt.ylabel("Concentração (mol/dm^3)")
+plt.rcParams['figure.figsize'] = (11, 7)
+plt.title("Concentração de reagentes por temo")
+plt.legend()
+plt.show()
+
+# Organizando os dados 
+dados_reacao = pd.DataFrame({"tempo(s)": t,"Ca": C[:,0], "Cb":C[:,1], "Cc":C[:,2], "Cd":C[:,3]})
+print(dados_reacao)
+dados_reacao.to_csv("dados_reacao.csv")
